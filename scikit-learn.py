9936d8ee3b90944ad455ca4aba28094c25fc73fe
hiro1103 <96195951+hiro1103@users.noreply.github.com>
2022-04-12 03:35:29 +0900

    更新

---
+++ scikit-learn.py
@@ -0,0 +1,238 @@
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import accuracy_score
+from matplotlib.colors import ListedColormap
+from distutils.version import LooseVersion
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+print('Class labels:', np.unique(y))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=1, stratify=y)
+
+print('Labels count in y:', np.bincount(y))
+print('Labels count in y_train:', np.bincount(y_train))
+print('Labels count in y_test:', np.bincount(y_test))
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+ppn = Perceptron(eta0=0.1, random_state=1)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified examples: %d' % (y_test != y_pred).sum())
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+print('Accuracy: %.3f' % ppn.score(X_test_std, y_test))
+
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.3, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.8,
+                    color=colors[idx],
+                    marker=markers[idx],
+                    label=cl,
+                    edgecolor='black')
+
+    if test_idx:
+        X_test, y_test = X[test_idx, :], y[test_idx]
+
+        if LooseVersion(matplotlib.__version__) < LooseVersion('0.3.4'):
+            plt.scatter(X_test[:, 0],
+                        X_test[:, 1],
+                        c='',
+                        edgecolor='black',
+                        alpha=1.0,
+                        linewidth=1,
+                        marker='o',
+                        s=100,
+                        label='test set')
+        else:
+            plt.scatter(X_test[:, 0],
+                        X_test[:, 1],
+                        c='none',
+                        edgecolor='black',
+                        alpha=1.0,
+                        linewidth=1,
+                        marker='o',
+                        s=100,
+                        label='test set')
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined,
+                      classifier=ppn, test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+plt.show()
+
+# ジグモイド関数を定義
+
+
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+
+
+# 0.1間隔で-7以上7未満のデータを生成
+z = np.arange(-7, 7, 0.1)
+# 生成したデータでジグモイド関数を実行
+phi_z = sigmoid(z)
+
+# 元のデータとジグモイド関数の出力をプロット
+plt.plot(z, phi_z)
+# 垂直線を追加(z=0)
+plt.axvline(0.0, color='k')
+# y軸の上限/下限を設定
+plt.ylim(-0.1, 1.1)
+# 軸のラベルの設定
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+# y軸の目盛を追加
+plt.yticks([0.0, 0.5, 1.0])
+# Axesクラスのオブジェクトの取得
+ax = plt.gca()
+# y軸の目盛に合わせて水平グリッド線を追加
+ax.yaxis.grid(True)
+# グラフを表示
+plt.tight_layout()
+plt.show()
+
+# y=1のコストを計算する関数
+
+
+def cost_1(z):
+    return -np.log(sigmoid(z))
+
+# y=0のコストを計算する関数
+
+
+def cost_0(z):
+    return -np.log(1-sigmoid(z))
+
+
+# 0.1間隔で-10以上10未満のデータを生成
+z = np.arange(-10, 10, 0.1)
+# ジグモイド関数を実行
+phi_z = sigmoid(z)
+# y=1のコストを計算する関数を実行
+c1 = [cost_1(x) for x in z]
+# 結果をプロット
+plt.plot(phi_z, c1, label='J(w) if y=1')
+# y=0のコストを計算する関数を実行
+c0 = [cost_0(x) for x in z]
+# 結果をプロット
+plt.plot(phi_z, c0, linestyle='--', label='J(w) if y=0')
+# x軸とy軸の上限/下限を設定
+plt.ylim(0.0, 5.1)
+plt.xlim([0, 1])
+# 軸のラベルを設定
+plt.xlabel('$\phi(z)$')
+plt.ylabel('J(w)')
+# 凡例の設定
+plt.legend(loc='upper center')
+# グラフを表示
+plt.tight_layout()
+plt.show()
+print()
+
+
+class LogisticRegressionGD(object):
+    # 勾配降下法に基づくロジスティック回帰分類器
+    # 学習率(0.0より大きく1.0以下の値)
+    eta: float
+    # 訓練データの訓練回数
+    n_iter: int
+    # 重みを初期化するための乱数シード
+    random_state: int
+    # w_:1次元配列(適用後の重み)
+    # cost_:リスト(各エポックでのロジスティクコスト関数)
+
+    def __init__(self, eta=0.05, n_iter=100, random_state=1) -> None:
+        # 学習率の初期化、訓練回数の初期化、乱数シードを固定にするrandom_state
+        self.eta = eta
+        self.n_iter = n_iter
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        # 訓練データに適合させる
+        # X:{配列のようなデータ構造},shape={n_examples,n_features},訓練データ
+        # y:{配列のようなデータ構造},shape={n_examples},目的変数
+        # 戻り値：object
+
+        rgen = np.random.RandomState(self.random_state)
+        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1+X.shape[1])
+        self.cost_ = []
+
+        # 訓練回数分まで訓練データを反復処理
+        for i in range(self.n_iter):
+            net_input = self.net_input(X)
+            output = self.activation(net_input)
+            errors = (y-output)
+            self.w_[1:] += self.eta*X.T.dot(errors)
+            self.w_[0] += self.eta*errors.sum()
+            # 誤差平方和のコストではなくロジスティクス回帰のコストを計算する
+            cost = -y.dot(np.log(output))-((1-y).dot(np.log(1-output)))
+            # エポックごとのコストを格納
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        # 総入力を計算
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def activation(self, z):
+        # ロジスティクシグモイド活性化関数を計算
+        return 1.0 / (1.0 + np.exp(-np.clip(z, -250, 250)))
+
+    def predict(self, X):
+        # 1ステップ後のクラスラベルを返す
+        return np.where(self.net_input(X) >= 0.0, 1, 0)
+
+
+X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
+y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
+# ロジスティック回帰のインスタンス化
+lrgd = LogisticRegressionGD(eta=0.05, n_iter=1000, random_state=1)
+# モデルを訓練データに適合させる
+lrgd.fit(X_train_01_subset, y_train_01_subset)
+# 決定領域をプロット
+plot_decision_regions(X=X_train_01_subset,
+                      y=y_train_01_subset, classifier=lrgd)
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
