ea70a43879ba31c73ed572ebe64202dc2081cdbd
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-12 14:07:30 +0900

    Add files via upload

--- scikit-learn.py
+++ scikit-learn.py
@@ -1,4 +1,4 @@
-
+from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -235,4 +235,48 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
+plt.show()
+
+# ロジスティック回帰のインスタンスを生成
+lr = LogisticRegression(C=100.0, random_state=1,
+                        solver='lbfgs', multi_class='ovr')
+# 訓練データをモデルに適合させる
+lr.fit(X_train_std, y_train)
+# 決定領域をプロット
+plot_decision_regions(X_combined_std, y_combined, classifier=lr,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+print(lr.predict_proba(X_test_std[:3, :]))
+print(lr.predict_proba(X_test_std[:3, :]).argmax(axis=1))
+print(lr.predict(X_test_std[:3, :]))
+print(lr.predict(X_test_std[0, :].reshape(1, -1)))
+
+# 空のリストを生成（重み係数、逆正規化パラメータ）
+weights, params = [], []
+# 10個の逆正規パラメータに対応するロジスティック回帰モデルをそれぞれ処理
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10.**c, random_state=1,
+                            solver='lbfgs', multi_class='ovr')
+
+    lr.fit(X_train_std, y_train)
+    # 重み係数を格納
+    weights.append(lr.coef_[1])
+    # 逆正規化パラメータを格納
+    params.append(10.**c)
+
+# 重み係数をNumpy配列に変換
+weights = np.array(weights)
+# 横軸に逆正則化パラメータ、縦軸に重み係数をプロット
+plt.plot(params, weights[:, 0], label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--', label='petal width')
+plt.ylabel('weight cofficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+# 横軸を対数スケールに設定
+plt.xscale('log')
 plt.show()
