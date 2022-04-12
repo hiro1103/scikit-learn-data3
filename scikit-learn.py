d816366d4a476b96933184feeb7f741b544c7aa5
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-12 16:18:34 +0900

    Add files via upload

--- scikit-learn.py
+++ scikit-learn.py
@@ -6,6 +6,7 @@ from sklearn.linear_model import Perceptron
 from sklearn.metrics import accuracy_score
 from matplotlib.colors import ListedColormap
 from distutils.version import LooseVersion
+from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib
@@ -279,4 +280,37 @@ plt.xlabel('C')
 plt.legend(loc='upper left')
 # 横軸を対数スケールに設定
 plt.xscale('log')
+plt.show()
+
+# 線形SVNのインスタンス化
+svm = SVC(kernel='linear', C=1.0, random_state=1)
+# 線形SVMのモデルに訓練データを適合させる
+svm.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=svm,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# 乱数シードを指定
+np.random.seed(1)
+# 標準正規分布に従う乱数で200行2列の行列を生成
+X_xor = np.random.randn(200, 2)
+# 2つの引数に対して排他的論理和を実行
+y_xor = np.logical_xor(X_xor[:, 0] > 0, X_xor[:, 1] > 0)
+# 排他的論理和の値が真の場合は1、偽の場合は-1を割り当てる
+y_xor = np.where(y_xor, 1, -1)
+# ラベル1を青のxでプロット
+plt.scatter(X_xor[y_xor == 1, 0], X_xor[y_xor == 1, 1],
+            c='b', marker='x', label='1')
+# ラベル-1を赤の四角でプロット
+plt.scatter(X_xor[y_xor == -1, 0], X_xor[y_xor == -1, 1],
+            c='r', marker='s', label='-1')
+# 軸の範囲を設定
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.tight_layout()
 plt.show()
