64fd00d044047f9748bad2b2f6f8fab1368fde4b
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-12 21:33:38 +0900

    Add files via upload

--- scikit-learn.py
+++ scikit-learn.py
@@ -1,3 +1,4 @@
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import tree
 from pydotplus import graph_from_dot_data
@@ -440,6 +441,18 @@ plot_decision_regions(X_combined, y_combined, classifier=forest,
                       test_idx=range(105, 150))
 plt.xlabel('petal length [cm]')
 plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+plt.show()
+
+# k最近傍法のインスタンスを生成
+knn = KNeighborsClassifier(n_neighbors=5, p=2, metric='minkowski')
+# 訓練データにk最近傍法のモデルを適合させる
+knn.fit(X_train_std, y_train)
+plot_decision_regions(X_combined_std, y_combined, classifier=knn,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
 plt.show()
