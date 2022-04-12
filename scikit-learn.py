56f783d2fcb8e2f56af90c3036e762b48c0a79d3
murakami <96195951+hiro1103@users.noreply.github.com>
2022-04-12 19:17:45 +0900

    Add files via upload

--- scikit-learn.py
+++ scikit-learn.py
@@ -1,3 +1,4 @@
+from regex import P
 from sklearn.linear_model import LogisticRegression
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
@@ -12,6 +13,7 @@ import numpy as np
 import matplotlib
 import warnings
 warnings.filterwarnings('ignore')
+
 iris = datasets.load_iris()
 X = iris.data[:, [2, 3]]
 y = iris.target
@@ -344,4 +346,53 @@ plt.xlabel('petal length [standardized]')
 plt.ylabel('petal width [standardized]')
 plt.legend(loc='upper left')
 plt.tight_layout()
+plt.show()
+
+# ジニ不純度の関数を定義
+
+
+def gini(p):
+    return (p)*(1-(p))+(1-p)*(1-(1-p))
+
+# エントロピーの関数を定義
+
+
+def entropy(p):
+    return -p*np.log2(p)-(1-p)*np.log2((1-p))
+
+# 分類誤差の関数を定義
+
+
+def error(p):
+    return 1-np.max([p, 1-p])
+
+
+# 確率を表す配列を生成(0-0.99まで0.01刻み)
+x = np.arange(0.0, 1.0, 0.01)
+# 配列の値をもとにエントロピー、分類誤差を計算
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e*0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+# 図の作成を開始
+fit = plt.figure()
+ax = plt.subplot(1, 1, 1)
+# エントロピー（2種)、ジニ不純度、分類誤差のそれぞれをループ処理
+for i, lab, ls, c in zip([ent, sc_ent, gini(x), err],
+                         ['Entropy', 'Entropy(scaled)', 'Gini impurity',
+                          'Misclassifcation error'],
+                         ['-', '-', '--', '-.'],
+                         ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+# 凡例の設定(中央の上に配置)
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+          ncol=5, fancybox=True, shadow=False)
+
+# 2本の水平の破線を引く
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+# 横軸の上限/下限を設定
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('impurity index')
 plt.show()
